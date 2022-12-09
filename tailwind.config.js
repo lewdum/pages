8b39c494f84289fca17354af382bed00132b3060
lewdum <lewdum@duck.com>
2022-12-09 01:20:19 -0300

    4.0

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,10 +5,21 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        'win-desktop': '#008080',
+        'win-box': '#c0c0c0',
+        'win-box-title': '#010180',
+        'win-box-border1': '#fefefe',
+        'win-box-border2': '#000000',
+        'win-box-text': '#212529',
+        'win-box-disabled': '#5c656d',
+      },
       fontFamily: {
         'term-mono': '"Major Mono Display", monospace',
         'term-logo': '"Courier New", Courier, monospace',
         'ubuntu': '"Ubuntu", sans-serif',
+        'win': '"MS Sans Serif", Tahoma, sans-serif',
+        'win-sys': 'System, "MS Sans Serif", Tahoma, sans-serif',
       }
     },
   },
